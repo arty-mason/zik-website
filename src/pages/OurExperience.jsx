317ce9cc873926b37179e81e5474ec1e2ff0cd66
ArtyomAmirdzhanyans <tyoma_manager@mail.ru>
2022-07-22 12:58:12 +0300

    Restyling Experience page, adding separate Achievements section, styling

--- src/pages/OurExperience.jsx
+++ src/pages/OurExperience.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 
-import { Box, CardContent, CardMedia, Paper, Typography } from "@mui/material";
+import { Box, Grid, Paper, Typography } from "@mui/material";
 
 import { theme } from "../config/color-config";
-
-import buildingsBackground from "../img/backgrounds/buildingsBackground.jpg";
-import styled from "@emotion/styled";
-
-const ExperienceText = styled(Typography)({});
+import { Colors } from "../config/design-config";
+import { achievements } from "../services/experience-achievements";
 
 const Experience = () => {
   return (
@@ -17,64 +14,71 @@ const Experience = () => {
         display: "flex",
         justifyContent: "space-around",
         alignItems: "center",
+        flexDirection: "column",
       }}
     >
-      <CardContent>
-        <Paper
-          elevation={20}
-          sx={{
-            padding: "10px",
-            borderRadius: "15px",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Typography
-            variant="h6"
-            component="h1"
-            sx={{
-              px: "20px",
-              color: theme.palette.primary.dark,
-              textAlign: "center",
-            }}
-          >
-            <b> В течение последних пяти лет наша компания:</b>
-          </Typography>
-          <Typography
-            variant="p"
-            component="p"
-            width="550px"
-            height="350px"
-            sx={{
-              px: "10px",
-              fontSize: "20px",
-              color: theme.palette.primary.main,
-            }}
-          >
-            - обслужила более 1000 лифтов на территории Украины;
-            <br /> <br />
-            - продолжает обслуживать 62 дома как управляющая компания;
-            <br /> <br />
-            - продала и смонтировала больше 100 лифтов в новостройках и
-            выполнила около 325 контрактов по ремонту лифтового оборудования;
-            <br /> <br />- выполнила более 500 договоров с учреждениями, которые
-            финансируются государственным бюджетом Украины.
-          </Typography>
-        </Paper>
-      </CardContent>
-      <CardMedia
-        component="img"
-        image={buildingsBackground}
-        alt="Lift service"
+      <Typography
+        variant="h4"
+        component="h1"
         sx={{
-          height: "400px",
-          width: "600px",
-          objectFit: "scale-down",
-          borderRadius: "15px",
+          px: "20px",
+          color: theme.palette.primary.dark,
+          textAlign: "center",
+          mb: "40px",
+          background: `linear-gradient(30deg, ${theme.palette.primary.dark}, ${Colors.Grey.light})`,
+          textFillColor: `transparent`,
+          backgroundClip: `text`,
         }}
-      />
+      >
+        <b> В течение последних пяти лет наша компания:</b>
+      </Typography>
+      <Grid container spacing={5}>
+        {achievements.map((achievement) => {
+          const IconComponent = achievement.icon;
+          return (
+            <Grid key={achievement.id} item md={3} xs={6}>
+              <Paper
+                elevation={20}
+                sx={{
+                  borderRadius: "15px",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "space-around",
+                  height: "300px",
+                  background: `linear-gradient(to bottom, ${theme.palette.primary.dark}, ${Colors.Grey.light})`,
+                  border: `5px solid ${Colors.Grey.light}`,
+                  "&:hover": {
+                    boxShadow: `3px 3px 5px 5px ${theme.palette.primary.light}`,
+                    cursor: "pointer",
+                    background: `linear-gradient(to bottom, ${theme.palette.primary.light}, ${Colors.Grey.dark})`,
+                  },
+                }}
+              >
+                <Typography
+                  sx={{
+                    p: "20px",
+                    fontSize: "16px",
+                    color: Colors.Grey.ultraLight,
+                    textAlign: "center",
+                    minHeight: "10px",
+                  }}
+                >
+                  {achievement.text}
+                </Typography>
+                <IconComponent
+                  sx={{
+                    width: "120px",
+                    height: "120px",
+                    color: Colors.Grey.ultraLight,
+                    pb: "10px",
+                  }}
+                />
+              </Paper>
+            </Grid>
+          );
+        })}
+      </Grid>
     </Box>
   );
 };
---
+++ src/services/experience-achievements.js
@@ -0,0 +1,33 @@
+import ElevatorIcon from '@mui/icons-material/Elevator';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import DoneAllIcon from '@mui/icons-material/DoneAll';
+
+export const achievements = [
+  {
+    id: `1`,
+    name: `Elevator`,
+    text: `Обслужила более 1000 лифтов на территории Украины`,
+    icon: ElevatorIcon,
+  },
+  {
+    id: `2`,
+    name: `Service`,
+    text: `Продолжает обслуживать 62 дома как управляющая компания`,
+    icon: SupervisorAccountIcon,
+  },
+  {
+    id: `3`,
+    name: `Supervise`,
+    text: `Продала и смонтировала больше 100 лифтов в новостройках и выполнила
+    около 325 контрактов по ремонту лифтового оборудования`,
+    icon: ShoppingCartIcon,
+  },
+  {
+    id: `4`,
+    name: `Completed`,
+    text: `Выполнила более 500 договоров с учреждениями, которые
+    финансируются государственным бюджетом Украины`,
+    icon: DoneAllIcon,
+  },
+]
